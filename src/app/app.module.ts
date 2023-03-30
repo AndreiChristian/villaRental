d2bcd0ffe87821f4c884cdfc3af862d4ab8774d3
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-30 21:19:51 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './website/home/home.component';
+import { PropertiesComponent } from './website/properties/properties.component';
+import { PropertiesListComponent } from './website/properties/properties-list/properties-list.component';
+import { PropertiesItemComponent } from './website/properties/properties-item/properties-item.component';
+import { ServicesComponent } from './website/properties/services/services.component';
+import { LoginComponent } from './website/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PropertiesComponent,
+    PropertiesListComponent,
+    PropertiesItemComponent,
+    ServicesComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
